02e04cd9a063d33e1738818f4f37aaa2ad4c698e
materliu <materliu@gmail.com>
2014-05-14 17:27:26 +0800

    add localhost exec script !

---
+++ app/scripts/localhost.js
@@ -0,0 +1,13 @@
+/**
+ * User: Liu Juguang(materliu@tencent.com)
+ * Date: 5/14/14
+ * Time: 5:23 PM
+ */
+
+(function() {
+    'use strict';
+
+    // test if localhost will execu when visit the localhost page
+
+    window.alert('materliu do your best, and win your life!');
+})();
